dfe8bf58a75200695f5a71f25e34026188b0dca6
Jonathan Milgrom <jmilgrom@snapchat.com>
2022-01-07 06:09:52 -0800

    boom

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const computeRolesByAction = require('./utils/rolesByAction');
+
+app.set('view engine', 'pug');
+
+const rolesByActions = computeRolesByAction();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/roles', (req, res) => {
+  const secureActionsQueryParam = req.query.secureActions;
+  if (!secureActionsQueryParam) {
+    res.send("'secureActions' query param is required");
+    return;
+  }
+
+  let secureActions;
+  try {
+    secureActions = secureActionsQueryParam.split(',');
+  } catch (e) {
+    res.send("Secure actions should be comma delimited like 'a,b,c'");
+    return;
+  }
+
+  console.info(`Secure actions requested ${secureActions}`);
+
+  res.render('rolesByAction', {
+    rolesByActions: secureActions.map(action => [action, rolesByActions[action]])
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ utils/rolesByAction.js
@@ -0,0 +1,25 @@
+const actionsByRoles = require("../assets/roles.json");
+const secureActions = require("../assets/secureActions.json");
+
+const roles = Object.keys(actionsByRoles);
+
+module.exports = () => {
+  const rolesByAction = {};
+  for (const action in secureActions) {
+    if (!rolesByAction[action]) {
+      rolesByAction[action] = [];
+    }
+
+    for (const role of roles) {
+      const actionsInRole = actionsByRoles[role];
+      if (!actionsInRole) {
+        console.warn(`No actions for ${role}`);
+        continue;
+      }
+      if (actionsInRole.indexOf(action) > -1) {
+        rolesByAction[action].push(role);
+      }
+    }
+  }
+  return rolesByAction;
+};
